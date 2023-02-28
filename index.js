756407c030c1027ea838ded11b396f4ccf702137
LeanTrey <treymclean0003@gmail.com>
2023-02-27 20:31:26 -0700

    all

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-let myLibrary = [];
+let myGoalLibrary = [];
 
-function Book() {
+function Goal(date, goal, complete) {
   // the constructor...
+  this.date = date
+  this.goal = goal
+  this.complete = complete
 }
 
-function addBookToLibrary() {
+function addGoalToLibrary() {
   // do stuff here
+  let date = document.querySelector('#day').value
+  let goal = document.querySelector('#goal').value
+  let complete = document.querySelector('#completed')
+  let newGoal = new Goal(date, goal, complete)
+  console.log(newGoal)
 }
+
+let newGoalBtn = document.querySelector('.button')
+newGoalBtn.addEventListener('click', function(){
+  let newGoalForm = document.querySelector('#daily-goals')
+  newGoalForm.style.display = 'block'
+})
+
+document.querySelector('#daily-goals').addEventListener('submit', function(){
+  event.preventDefault()
+  addGoalToLibrary()
+})
